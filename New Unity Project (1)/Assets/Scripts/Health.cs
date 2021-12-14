979679881ad04c17d69f10fdbd89ad9be34460ba
jerry0109200 <108051017@live.asia.edu.tw>
2021-12-14 16:03:00 +0800

    1214
    1214

--- New Unity Project (1)/Assets/Scripts/Health.cs
+++ New Unity Project (1)/Assets/Scripts/Health.cs
@@ -6,6 +6,9 @@ public class Health : MonoBehaviour
 {
     public GameObject pickE;
 
+    //【產生音效 1】
+    public AudioClip audioClip;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Instantiate(pickE, gameObject.transform.position, Quaternion.identity);
@@ -15,6 +18,10 @@ public class Health : MonoBehaviour
         print("碰到的東西是 :" + rubyGo);
 
         rubyGo.ChangeHealth(1);
+
+        //【產生音效 2】
+        rubyGo.PlaySound(audioClip);
+
         Destroy(gameObject);
     }
 }
